3fd368e098d500356d3176d68ac98931753bd1b9
RomanLatipov <romanlatipov@gmail.com>
2024-09-23 23:24:39 -0400

    create input field for search

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-async function getKey() {
-    const response = await fetch("http://localhost:5555/key")
-    const data = await response.json();
-    getData(data.key)
-}
 
-async function getData(key) {
+
+async function submit() {
+    const response = await fetch("http://localhost:5555/key")
+    const secretKey = await response.json();
+    const key = secretKey.key;
+
     const repsonse = fetch(`https://www.googleapis.com/customsearch/v1?key=${key}&cx=partner-pub-3853029306847841:xz1zltlp4uh&q=House Of The Dragon`);
     const data = (await repsonse).json();
     console.log(data);
-}
 
-getKey();
+}
