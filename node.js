6ca8574cfbfd79c716fb01cd488afdd0e483b4dc
RomanLatipov <romanlatipov@gmail.com>
2024-09-23 23:14:05 -0400

    create fetch for google api

--- node.js
+++ node.js
@@ -4,16 +4,15 @@ import dotenv from 'dotenv';
 
 const app = express()
 app.use(cors());
+app.use(express.json());
+
 dotenv.config();
 const key = process.env.SECRET_KEY;
-console.log(key)
-
-const port = 5555;
 
 app.get('/key', (req, res) => {
-    res.send(key)
+    res.send({"key": key});
 });
 
 app.listen(5555, () => {
-    console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${5555}`)
 })
--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-console.log("hello world")
-fetch("http://localhost:5555/key")
-.then(res => res.json())
-.then(data => console.log(data))
+async function getKey() {
+    const response = await fetch("http://localhost:5555/key")
+    const data = await response.json();
+    getData(data.key)
+}
+
+async function getData(key) {
+    const repsonse = fetch(`https://www.googleapis.com/customsearch/v1?key=${key}&cx=partner-pub-3853029306847841:xz1zltlp4uh&q=House Of The Dragon`);
+    const data = (await repsonse).json();
+    console.log(data);
+}
+
+getKey();
