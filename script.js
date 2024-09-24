6e521edd4f4934ab3bcc80c9e06ebfe314669dbc
RomanLatipov <romanlatipov@gmail.com>
2024-09-23 23:37:32 -0400

    display results on page

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-
-
 async function submit() {
     const response = await fetch("http://localhost:5555/key")
     const secretKey = await response.json();
     const key = secretKey.key;
 
-    const repsonse = fetch(`https://www.googleapis.com/customsearch/v1?key=${key}&cx=partner-pub-3853029306847841:xz1zltlp4uh&q=House Of The Dragon`);
-    const data = (await repsonse).json();
-    console.log(data);
+    const search = await fetch(`https://www.googleapis.com/customsearch/v1?key=${key}&cx=partner-pub-3853029306847841:xz1zltlp4uh&q=House Of The Dragon`);
+    const data = await search.json();
+    const res = data;
+    try {
+        for (i = 0; i< res.items.length; i++) {
+            document.getElementById('content').innerHTML +=
+            `<form>
+                <a href=${res.items[i].link} target="_blank">${res.items[i].link}</a>
+                <a target="_blank" href=${res.items[i].link}>
+                    <h2>${res.items[i].title}</h2>
+                </a>
+                <a>${res.items[i].htmlSnippet}</a><br>
+            </form>`;
+        }
+    } catch (error) {
+        document.getElementById('content').innderHTML = '';
+    }
 
 }
